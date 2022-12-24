9139fd99edda9ada4edb7d4fc1459df4c9067c2f
Yoann Gaiero <ygaiero@c1r4p4.42nice.fr>
2022-12-24 22:49:48 +0100

    edit makefile rules to rm volumes and script for mariadb

--- Makefile
+++ Makefile
@@ -7,4 +7,8 @@ stop:
 	docker rmi srcs_mariadb
 	docker rmi srcs_wordpress
 
+rmVolumes:
+	sudo rm -rf ../../wordpress
+	sudo rm -rf ../../mariadb
+
 .phony: all stop     
--- srcs/requirements/mariadb/tools/configdb.sh
+++ srcs/requirements/mariadb/tools/configdb.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-if [ ! -f "/var/lib/mysql/mysql" ]; then
-#auto-completion
-printf "\n Y\n Y\n ${MYSQL_ROOT_PASSWORD}\n${MYSQL_ROOT_PASSWORD}\n Y\n Y\n Y\n Y\n" | mysql_secure_installation
+if [ ! -d "/var/lib/mysql/db" ]; then
 #createdb and add privileges (with grand option == can give privileges)
-
 mysql -e "CREATE DATABASE ${MYSQL_DATABASE};CREATE USER '${MYSQL_USER_ADMIN}'@localhost IDENTIFIED BY '${MYSQL_PASSWORD_ADMIN}';GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER_ADMIN}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD_ADMIN}' WITH GRANT OPTION;FLUSH PRIVILEGES;"
 mv ./etc/mariadb-server.cnf ./etc/my.conf.d/
 echo "MariaDB installed."
